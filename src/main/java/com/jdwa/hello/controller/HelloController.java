f2e90fd7b159df7ba522b7e8eb81d3dd57aa968a
caozhou_zch <caozhou_zch@163.com>
2020-05-30 21:15:33 +0800

    关于@Controller与@ResponseBody

--- src/main/java/com/jdwa/hello/controller/HelloController.java
+++ src/main/java/com/jdwa/hello/controller/HelloController.java
@@ -4,15 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by czz on 2020/5/30.
  */
-@Controller
+@RestController  //此注解为@ResponseBody与@Controller的合体
+//@ResponseBody  //将此注解放在类上，表示该类所有方法均为resetful风格
+//@Controller
 @RequestMapping("/hello")
 public class HelloController {
 
-    @ResponseBody
+//    @ResponseBody
     @RequestMapping("/sout")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name){
         return String.format("Hello %s!", name);
