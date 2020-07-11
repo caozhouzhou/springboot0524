d7b773abd931d5d8da7fca259b617469458be409
caozhou_zch <caozhou_zch@163.com>
2020-07-11 10:49:37 +0800

    配置文件注入对象属性，以及测试方法

--- src/test/java/com/jdwa/SpringbootApplicationTests.java
+++ src/test/java/com/jdwa/SpringbootApplicationTests.java
@@ -1,13 +1,22 @@
 package com.jdwa;
 
+import com.jdwa.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 class SpringbootApplicationTests {
 
+	@Autowired
+	Person person;
+
 	@Test
 	void contextLoads() {
+		System.out.println(person);
 	}
 
 }
